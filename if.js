68a07b936e747da27229e2f4e701e983f4c754af
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-27 11:32:18 +0600

    if else condition

---
+++ if.js
@@ -0,0 +1,49 @@
+/**
+ * Tui jodi amake khawas
+ * taile ami asbo
+ * 
+ * 
+ * --------------
+ * if you buy food for me i will come
+ * 
+ * 
+ * 
+ * ---------Structure----------
+ * if (condition){
+ * 
+ * }
+ * 
+ * ----------------------------------
+ * 
+ */
+
+// if(3 < 10){
+//     console.log('I have a smaller value');
+// }
+// if(13 < 10){
+//     console.log('I have a smaller value');
+// }
+
+
+// var biryaniPrice = 200;
+
+// if(biryaniPrice < 300){
+//     console.log('mama give me some biryani');
+// }
+
+
+/**
+ * if (condition){
+ * execute code if condition above is true
+ * }else{
+ *  not execute code
+ * }
+ */
+
+
+var weight = 20;
+if(weight <= 20){
+    console.log('I will carry it by meself');
+}else{
+    console.log('i will rent a rickshaw');
+}
