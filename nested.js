9b98d8773bfa7806cbc1fd1e750e9139e780b696
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-27 19:17:09 +0600

    if else condition in if else condition apply

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = 0;
+
+if(money > 300){
+    console.log('bro you are rich!');
+}else{
+    if(money > 150){
+        console.log('tui gorib o na boro lok o na');
+    }
+    else{
+        if(money > 0){
+            console.log('dosto kola kha');
+        }
+        else{
+            console.log('tui amar dosto na');
+        }
+    }
+}
