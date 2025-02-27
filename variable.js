de74dd5b4431f07e756881644d9e38214f5fe4d9
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-27 10:57:53 +0600

    add javacript comparison rules

--- variable.js
+++ variable.js
@@ -24,3 +24,44 @@ age = 35;
 
 
 console.log(age);
+
+
+// Recap
+
+/**
+ * Variable => Container
+ * 
+ */
+
+// number try variable
+var sunglassPrice = 80;
+
+// string
+var subject = "CSE";
+
+// boolean
+var passed = true;
+
+
+
+/**
+ * Naming convension
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+
+var lastExamResult = 89;
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=, %
+ * 3. isNaN
+ * 4. null
+ * 5. number
+ */
