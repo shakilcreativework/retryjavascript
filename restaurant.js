5b25873e72fce5e8b2f6c8d98578cba981d70175
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-27 19:08:47 +0600

    again more try to solve complex if, else if and else

---
+++ restaurant.js
@@ -0,0 +1,24 @@
+const price = 500;
+const age = 45;
+
+if(age <= 12){
+    console.log('You can eat for free');
+}else if(age >= 60){
+    // 50% discount
+    const discount = price * 50 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}else if(age >= 50 ){
+    // 25% discount
+    const discount = price * 25 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}else if(age >= 40 ){
+    // 10% discount
+    const discount = price * 10 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else{
+    console.log(price);
+}
