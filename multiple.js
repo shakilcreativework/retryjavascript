3f57d4cce3c82e6c64ed8ca5707a4b99607edbc5
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-27 17:54:14 +0600

    multiple condition use && and ||

--- multiple.js
+++ multiple.js
@@ -1,8 +1,40 @@
-const salary = 16000;
+const salary = 26000;
 const isBSC = true;
+const height = 78;
+const hasCar = false;
 
-if(salary > 20000){
-    console.log('Su---------patro');
+// if(salary > 20000 && 66){
+//     console.log('Su---------patro');
+// }else{
+//     console.log('onno patro khuji');
+// }
+
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso baba kobul');
+// }else{
+//     console.log('bag tui mokbul');
+// }
+
+
+
+// if(salary > 25000 || height > 72 || isBSC == true){
+//     console.log('eso baba kobul');
+// }else{
+//     console.log('bag tui mokbul');
+// }
+
+
+
+// --------------Complex condition--------------------
+// if((salary > 25000 && hasCar == true) || isBSC == true){
+//     console.log('tomar 14 gosti raji');
+// }else{
+//     console.log('tomar sopno pure hobe na sona');
+// }
+
+if((salary > 25000 || hasCar == true) && isBSC == true){
+    console.log('tomar 14 gosti raji');
 }else{
-    console.log('onno patro khuji');
+    console.log('tomar sopno pure hobe na sona');
 }
