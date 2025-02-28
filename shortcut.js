47027ee2a73dc0af7b15bfe0f6990a8c12059c25
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 11:08:49 +0600

    What is ! meaning?

---
+++ shortcut.js
@@ -0,0 +1,30 @@
+const isLeader = true;
+
+// if(isLeader === true){
+//     console.log('leader aschhe! chair charo');
+// }else{
+//     console.log('vitore dukte dibi na');
+// }
+
+// if(isPassed === true)
+// if(isLeader){
+//     console.log('leader aschhe! chair charo');
+// }else{
+//     console.log('vitore dukte dibi na');
+// }
+
+
+const isPassed = false;
+
+// if(isPassed === false){
+//     console.log('toke biye diye dimu');
+// }else{
+//     console.log('tumi amader noyon er moni');
+// }
+
+// if(isPassed === false)
+if(!isPassed){
+    console.log('toke biye diye dimu');
+}else{
+    console.log('tumi amader noyon er moni');
+}
